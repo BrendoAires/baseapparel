0d2b06d8800d4939e18981f913f4d024622fa363
BrendoAires <brendoc.aires@gmail.com>
2021-11-19 14:32:39 -0300

    primeiro commit

---
+++ script/script.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector('#button');
+const  email = document.getElementById("email").value;
+const inputemail = document.querySelector('#email');
+btn.addEventListener('click', pegaCampo)
+function pegaCampo() {
+    let btn = document.getElementById("button");
+    let email = document.getElementById("email").value;
+    
+    validarEmail(email);
+
+}
+
+
+function validarEmail(emailCapturadoDoFormulario){
+    let validarREGexNoEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (emailCapturadoDoFormulario.match(validarREGexNoEmail)){
+        
+        document.getElementById("msgErro").style.display = "none";
+        document.getElementById("email").value = "";
+        return true;
+
+    } else{
+        
+        inputemail.classList.add('inputerro');
+        document.getElementById("msgErro").style.color = "red";
+        
+        document.getElementById("msgErro").style.display = "block"; 
+        
+        return false;
+
+    }
+
+}
+    
+    function limpaErro(){
+    document.getElementById("msgErro").style.display = "none";
+    inputemail.classList.remove('inputerro');
+    }
